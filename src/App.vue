3f3b6c458e95b6cbd062d1d78c391cb6042b1bdb
Joseph_rey <72607892+abi-collab@users.noreply.github.com>
2025-04-20 00:20:49 +0800

    upsxxx

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<script setup></script>
+<script setup>
+import bgFlat from '@/assets/flat/38549135_8638026.svg';
+</script>
 
 <template>
     <div>
         <router-view />
     </div>
+    <img :src="bgFlat" alt="Background" class="absolute top-16 left-0 w-full h-full object-cover opacity-5" style="z-index: -1" />
 </template>
 
 <style scoped></style>
--- src/layout/AppSidebar.vue
+++ src/layout/AppSidebar.vue
@@ -3,11 +3,8 @@ import AppMenu from './AppMenu.vue';
 </script>
 
 <template>
-    <div class="layout-sidebar grid place-content-between">
+    <div class="layout-sidebar">
         <app-menu></app-menu>
-        <!-- <div class="flex justify-center w-full">
-            <iframe class="w-full" src="https://lottie.host/embed/1b65ded5-7e25-4e3b-aee6-3b993f584e5d/FPGxrnWD17.lottie"></iframe>
-        </div> -->
     </div>
 </template>
 
